fa1c0722ffbff18ed552e233627bab29c59475c3
Cee-nthy <cynthiaolando2020@gmail.com>
2023-02-16 15:33:54 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This function prints a message to the console using the
+ *              puts() function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
