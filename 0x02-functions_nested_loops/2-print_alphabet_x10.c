50044854ac55c49d43bc2bf3be8b0ed1d6dbf2fb
Cee-nthy <cynthiaolando2020@gmail.com>
2023-02-27 11:18:06 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int count;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
