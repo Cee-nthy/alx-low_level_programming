50954005bb64195e606fb63e37b71088c2503222
Cee-nthy <cynthiaolando2020@gmail.com>
2023-02-28 07:30:49 -0800

    Multiple mains

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * positive_or_negative - checks whether the input is +ve, -ve or zero
+ * @i: The integer to be checked
+ * return: 0
+ */
+
+void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */
