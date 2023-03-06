49d4b0e8b988607c63d707cd317c7bccb0a17207
Cee-nthy <cynthiaolando2020@gmail.com>
2023-03-06 01:55:39 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -23,17 +23,3 @@ char *_memset(char *s, char b, unsigned int n)
 
 	return (s);
 }
-
-/* pointer arithmetic version
-char *_memset(char *s, char b, unsigned int n)
-{
-	int size = n;
-	if (size > 0)
-	{
-		int i;
-		for (i = 0; i < size; i++)
-		     *(s + i) = b;
-	}
-	return (s);
-}
-*/
